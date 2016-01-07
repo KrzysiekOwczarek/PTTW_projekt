d5d24117fa8b5fa46f4cc0830980be899e949602
Radoslaw Jarzynka <radoslaw.jarzynka@gmail.com>
2016-01-07 19:36:31 +0100

    Utworzenie kodera i dekodera splotowego

---
+++ PTTW_PROJEKT.m
@@ -0,0 +1,58 @@
+%% Menu g³ówne
+disp('Witamy w projekcie z PTTW - Koder i Dekoder splotowy');
+disp('Autorzy - Rados³aw Jarzynka & Krzysztof Owczarek');
+disp('Wybierz tryb pracy:')
+disp('(1) - Zakodowanie pliku')
+disp('(2) - Dekodowanie pliku (hard)')
+disp('(3) - Dekodowanie pliku (unquantized)')
+
+%% Wczytanie pliku
+chosenMode = input('Wybierz tryb:');
+while (chosenMode ~= 1 && chosenMode ~= 2 && chosenMode ~= 3)
+    chosenMode = input('Wpisano z³± warto¶æ, wybierz ponownie:');
+end
+disp('Wpisz nazwê pliku z danymi')
+filename = input('Wspierany format to znaki 0 lub 1 oddzielone znakiem nowej linii (\\n): ', 's');
+fileId = fopen(filename);
+while fileId == -1
+    filename = input('Wpisz poprawn± nazwê pliku: ', 's');
+    fileId = fopen(filename);
+end
+data_array = fscanf(fileId ,'%d');
+
+%% Utworzenie trellis
+trel =  poly2trellis([5],[23 35 0]);
+% trackback - do dekodowania, zwykle 3-krotno¶æ enkodera (u nas 1/3 wiêc 3*3 = 9)
+tblen = 1; 
+%% enkodowanie/dekodowanie
+switch chosenMode
+    case 1
+        result = convenc(data_array,trel);
+    case 2
+        % je¿eli jest z³a d³ugo¶æ danych (nie jest podzielna przez 3)
+        % appendujemy zera by siê kod nie wywali³
+        while (mod(size(data_array), 3) ~= 0)
+            data_array = [data_array; 0];
+        end
+        result = vitdec(data_array,trel,tblen,'cont','hard');
+        %usuwamy pierwszy element (przez trackback jest on zerowy)
+        result = result(2:length(result));
+        result = [result; 0];
+    case 3
+        while (mod(size(data_array), 3) ~= 0)
+            data_array = [data_array; 0];
+            result = [result; 0];
+        end
+        ucode = 1-2*data_array;
+        result = vitdec(ucode',trel,tblen,'cont','unquant');
+        result = result(2:length(result));
+        result = [result; 0];
+    otherwise
+        disp('Wybrano z³y tryb!');
+end
+
+edit result.txt
+resultFileID = fopen('result.txt','w');
+fprintf(resultFileID,'%d\n',result);
+fclose(resultFileID);
+disp('Zrobione!')
